66ff7f506d33f562374a8a551276347b413de37e
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-06 16:20:20 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+var name1 = 'asif';
+var capitalize = name1.charAt(0).toUpperCase(); // first letter is capital
+console.log("capitalize is ".concat(capitalize).concat(name1.slice(1))); // other letter will samll letters 
+console.log('name in lower case', name1.toLowerCase());
+console.log('name in upper case', name1.toUpperCase());
---
+++ main.ts
@@ -0,0 +1,8 @@
+let name1: string = 'asif';
+
+let capitalize: string = name1.charAt(0).toUpperCase(); // first letter is capital
+
+console.log(`capitalize is ${capitalize}${name1.slice(1)}`);    // other letter will samll letters 
+
+console.log('name in lower case', name1.toLowerCase());
+console.log('name in upper case', name1.toUpperCase());
